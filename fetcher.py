dfae6f88976e1855aeb426738c8e74d94448c4d9
Yurii Holodnyi <holodnyijurii@gmail.com>
2017-11-15 23:12:13 +0100

    Fetching attendances

--- fetcher.py
+++ fetcher.py
@@ -4,6 +4,7 @@ from requests import Session
 class MeetupFetcher(object):
     EVENTS_URL_FORMAT = "https://api.meetup.com/{0}/events"
     MEMBERS_URL_FORMAT = "https://api.meetup.com/{0}/members"
+    ATTENDANCES_URL_FORMAT = "https://api.meetup.com/{0}/events/{1}/attendance"
 
     def __init__(self, meetup_name, token_file=".token"):
         self._headers = {}
@@ -14,21 +15,18 @@ class MeetupFetcher(object):
 
     def last_events_ids(self, number_of_events=1):
         response = self._session.get(self.EVENTS_URL_FORMAT.format(self._meetup_name), params={"status": "past"})
-        if response.status_code != 200:
-            raise Exception("Status code different than 200, something went wrong. Maybe just try again?")
+        self._check_response(response)
         return [event["id"] for event in sorted(response.json(), key=lambda e: int(e["created"]))[-number_of_events:]]
 
     def members(self):
         response = self._session.get(self.MEMBERS_URL_FORMAT.format(self._meetup_name))
-        if response.status_code != 200:
-            raise Exception("Status code different than 200, something went wrong. Maybe just try again?")
+        self._check_response(response)
         return [(member["id"], member["name"]) for member in response.json()]
 
-    def attendance_list(self, event):
-        return [
-            {"member": {"id": 11774000}, "status": "noshow"},
-            {"member": {"id": 65724362}, "status": "attended"},
-        ]  # TODO
+    def attendance_list(self, event_id):
+        response = self._session.get(self.ATTENDANCES_URL_FORMAT.format(self._meetup_name, event_id))
+        self._check_response(response)
+        return response.json()
 
     def _load_headers(self, token_file):
         try:
@@ -36,3 +34,7 @@ class MeetupFetcher(object):
                 self._headers['App-Token'] = app_file.readline().strip()
         except FileNotFoundError:
             raise Exception("Please create file with app token")
+
+    def _check_response(self, response):
+        if response.status_code != 200:
+            raise Exception("Status code {0} is different than 200, something went wrong.".format(response.status_code))
