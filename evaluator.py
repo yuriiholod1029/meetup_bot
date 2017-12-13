563c1391e3def509b3b20f7a8953cfe181116692
Yurii Holodnyi <holodnyijurii@gmail.com>
2017-12-13 13:18:35 +0100

    Support for fetching single event

--- evaluator.py
+++ evaluator.py
@@ -9,15 +9,25 @@ class Evaluator(object):
         events_ids = self._fetch.last_events_ids(self._num_of_events)
         reputation = reputation_class(self._fetch.members())
 
-        for event in events_ids:
-            attendances = self._fetch.attendance_list(event)
-            for attendance in attendances:
-                if attendance["member"]["id"] == 0:
-                    continue  # case when member is a former member
-                reputation_affect = self._evaluate_attendance(attendance["status"])
-                reputation.update_member_evaluation(attendance["member"]["id"], reputation_affect)
+        for event_id in events_ids:
+            self._evaluate_event(reputation, event_id)
 
         return reputation
+
+    def evaluate_single_event(self, reputation_class, event_id):
+        reputation = reputation_class(self._fetch.members())
+
+        self._evaluate_event(reputation, event_id)
+
+        return reputation
+
+    def _evaluate_event(self, reputation_collector, event_id):
+        attendances = self._fetch.attendance_list(event_id)
+        for attendance in attendances:
+            if attendance["member"]["id"] == 0:
+                continue  # case when member is a former member
+            reputation_affect = self._evaluate_attendance(attendance["status"])
+            reputation_collector.update_member_evaluation(attendance["member"]["id"], reputation_affect)
 
     def _evaluate_attendance(self, attendance_status):
         return self._config.evaluate(attendance_status)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import sys
 
 from config import JsonFileConfig
@@ -13,12 +14,20 @@ if __name__ == "__main__":
     parser.add_argument("--meetup", help="meetup name", default="AgileWarsaw")
     parser.add_argument("--config", help="config file name", default="config.json")
     parser.add_argument("--last", help="number of last events to check", default="10", type=int)
+    parser.add_argument("--eventid", help="for downloading declarations before this event", type=int)
     args = parser.parse_args(sys.argv[1:])
 
     fetch = MeetupFetcher(args.meetup)
     configuration = JsonFileConfig(args.config)
     configuration.load()
 
-    reputation = Evaluator(configuration, fetch, args.last).evaluate_by_events(Reputation)
+    if hasattr(args, 'eventid') and args.eventid:
+        logging.warning("Running in declarations fetching mode, event={}".format(args.eventid))
+        reputation = Evaluator(configuration, fetch).evaluate_single_event(Reputation, args.eventid)
+        result_file_name = "{0}-{1}".format(args.meetup, args.eventid)
+    else:
+        logging.warning("Running in general repuptatons counting mode")
+        reputation = Evaluator(configuration, fetch, args.last).evaluate_by_events(Reputation)
+        result_file_name = "result.csv"
 
-    Dumper("result.csv", reputation).dump_to_csv()
+    Dumper(result_file_name, reputation).dump_to_csv()
