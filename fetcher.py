eeb74a965d2a5651fd7f02d96d3dfd70382208cc
Yurii Holodnyi <holodnyijurii@gmail.com>
2017-12-14 22:30:22 +0100

    Fetching events 48-72 hours before their start date

--- fetcher.py
+++ fetcher.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from requests import Session
 
 
@@ -16,14 +17,7 @@ class MeetupFetcher(object):
 
     def last_events_ids(self, number_of_events=1):
         params = {"status": "past", 'key': self._get_token()}
-        events = (
-            event
-            for response in self._all_responses(
-                self.EVENTS_URL_FORMAT.format(self._meetup_name),
-                params=params
-            )
-            for event in response.json()
-        )
+        events = self._events_according_to_params(params)
         return [event["id"] for event in sorted(events, key=lambda e: int(e["created"]))[-number_of_events:]]
 
     def members(self):
@@ -43,6 +37,31 @@ class MeetupFetcher(object):
                 self.ATTENDANCES_URL_FORMAT.format(self._meetup_name, event_id), params=params)
             for attendance in response.json()
         ]
+
+    def upcoming_ids_in_time_deltas_range(self, max_before=timedelta(hours=72), min_before=timedelta(hours=48)):
+        earliest_possible = datetime.now() + min_before
+        latest_possible = datetime.now() + max_before
+        assert earliest_possible < latest_possible
+        upcoming_events = self._upcoming_events()
+        return [
+            upcoming["id"]
+            for upcoming in upcoming_events
+            if earliest_possible <= datetime.fromtimestamp(float(upcoming["time"]) / 1000.0) <= latest_possible
+        ]
+
+    def _upcoming_events(self):
+        params = {"status": "past", 'key': self._get_token()}
+        return self._events_according_to_params(params)
+
+    def _events_according_to_params(self, params):
+        return (
+            event
+            for response in self._all_responses(
+                self.EVENTS_URL_FORMAT.format(self._meetup_name),
+                params=params
+            )
+            for event in response.json()
+        )
 
     def _all_responses(self, initial_request, **kwargs):
         response = self._session.get(initial_request, **kwargs)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import os
 import sys
 
 from config import JsonFileConfig
@@ -14,22 +15,27 @@ def read_args():
     parser.add_argument("--meetup", help="meetup name", default="AgileWarsaw")
     parser.add_argument("--config", help="config file name", default="config.json")
     parser.add_argument("--last", help="number of last events to check", default="10", type=int)
-    parser.add_argument("--eventid", help="for downloading declarations before this event", type=int)
+    parser.add_argument("--eventid", help="for downloading declarations for this event", type=int)
+    parser.add_argument("-d", help="fetch declarations to temporal files if not fetched yet", action='store_true')
     return parser.parse_args(sys.argv[1:])
 
 
 def evaluate_reputations(args):
+    # TODO do it using temporal files with declarations
     logging.warning("Running in general repuptatons counting mode")
     reputation = Evaluator(configuration, fetch, args.last).evaluate_by_events(Reputation)
     result_file_name = "result.csv"
     ReputationDumper(result_file_name, reputation).dump_to_csv()
 
 
-def fetch_single_event(args):
-    logging.warning("Running in declarations fetching mode, event={}".format(args.eventid))
-    attendances = fetch.attendance_list(args.eventid)
-    result_file_name = "{0}-{1}.csv".format(args.meetup, args.eventid)
+def fetch_single_event(event_id, result_file_name):
+    logging.warning("Running in declarations fetching mode, event={}".format(event_id))
+    attendances = fetch.attendance_list(event_id)
     DeclarationsDumper(result_file_name, attendances).dump_to_csv()
+
+
+def meetup_event_file_name(meetup_name, event_id):
+    return "{0}-{1}.csv".format(meetup_name, event_id)
 
 
 if __name__ == "__main__":
@@ -39,6 +45,15 @@ if __name__ == "__main__":
     configuration.load()
 
     if hasattr(args, 'eventid') and args.eventid:
-        fetch_single_event(args)
+        fetch_single_event(args.eventid, meetup_event_file_name(args.meetup, args.eventid))
+    elif hasattr(args, "d"):
+        for event_id in fetch.upcoming_ids_in_time_deltas_range():
+            logging.warning(event_id)
+            result_file_name = meetup_event_file_name(args.meetup, event_id)
+            logging.warning(result_file_name)
+            if not os.path.exists(result_file_name):
+                fetch_single_event(event_id, result_file_name)
+            else:
+                logging.warning("Declarations data for event %a already dumped to file", event_id)
     else:
         evaluate_reputations(args)
