2c681d8bb95b8ea5d5691ec8fffa7f98ff7f484c
Yurii Holodnyi <holodnyijurii@gmail.com>
2018-03-14 03:46:39 +0100

    Stop warnings on zero-rated config options

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class JsonFileConfig(object):
         config_key = "{0}, {1}".format(declaration, attendance)
         grade = self._config.get(config_key)
 
-        if not grade:
-            logging.warning("Unknown attendances composition: %s", config_key)
+        if grade is None:
+            logging.warning("Unknown attendances composition: %r", config_key)
             return 0
 
         return grade
