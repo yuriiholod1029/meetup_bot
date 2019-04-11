6a97bfaaa3f5a6ca633cdbc59189cc3d449f1fdd
Yurii Holodnyi <holodnyijurii@gmail.com>
2019-04-11 16:09:19 +0300

    add type hints and make minor improvements to the code in evaluator module

--- app/src/meetup_bot/reputation/evaluator.py
+++ app/src/meetup_bot/reputation/evaluator.py
@@ -1,17 +1,19 @@
 import logging
 from typing import Dict
 
+from meetup_bot.reputation.fetcher import MeetupFetcher
+
 
 class Evaluator(object):
 
-    def __init__(self, reputation_score: Dict, fetch, num_of_events=1):
+    def __init__(self, reputation_score: Dict, fetcher: MeetupFetcher, num_of_events: int = 1):
         self.REPUTATION_SCORE = reputation_score
-        self._fetch = fetch
-        self._num_of_events = num_of_events
+        self.fetcher = fetcher
+        self.num_of_events = num_of_events
 
     def evaluate_by_events(self, reputation_class):
-        events_ids = self._fetch.last_events_ids(self._num_of_events)
-        reputation = reputation_class(self._fetch.members())
+        events_ids = self.fetcher.last_events_ids(self.num_of_events)
+        reputation = reputation_class(self.fetcher.members())
 
         for event_id in events_ids:
             self._evaluate_event(reputation, event_id)
@@ -19,15 +21,15 @@ class Evaluator(object):
         return reputation
 
     def _evaluate_event(self, reputation_collector, event_id):
-        attendances = self._fetch.attendance_list(event_id)
+        attendances = self.fetcher.attendance_list(event_id)
         for attendance in attendances:
-            member = attendance.get("member")
+            member = attendance.get('member')
             if member is None:
                 continue  # future meetup, no attendance list?
-            if attendance["member"]["id"] == 0:
+            if attendance['member']['id'] == 0:
                 continue  # case when member is a former member
             reputation_affect = self._evaluate_attendance(attendance)
-            reputation_collector.update_member_evaluation(attendance["member"]["id"], reputation_affect)
+            reputation_collector.update_member_evaluation(attendance['member']['id'], reputation_affect)
 
     def _evaluate_attendance(self, attendance: Dict):
         rsvp = attendance.get('rsvp')
